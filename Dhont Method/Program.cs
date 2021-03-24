74865f0c0b013927e3c6cb7cee12e1f184d83201
sibs <sibson_16@hotmail.com>
2021-03-24 23:49:08 +0000

    Add project files.

---
+++ Dhont Method/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Dhont_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // file path to text file o
+            string filepath = @"Assessment1Data.txt";
+
+
+
+
+
+    }    }
+}
