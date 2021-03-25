430cd06249bcf824ecee43fed19dfb1eccc20f3b
sibert16 <69308469+sibert16@users.noreply.github.com>
2021-03-25 00:43:00 +0000

    Update Program.cs
    Put each party on a list 
    stores value in a list of string
    users to enter the threshold

--- Dhont Method/Program.cs
+++ Dhont Method/Program.cs
@@ -8,6 +8,34 @@ namespace Dhont_Method
         {
             // file path to text file o
             string filepath = @"Assessment1Data.txt";
+             // Store values in a list of string
+            List<string> file = File.ReadAllLines(filepath).ToList();
+
+            // Puts each party into a list 
+            List<Party> partys = new List<Party>();
+            foreach (string line in file)
+            {
+                string[] items = line.Split(',');
+                Party pobj = new Party(items[0], Convert.ToInt32(items[1]));
+                partys.Add(pobj);
+            }
+
+            // User to enter threshold. Converts string to int 
+            Console.WriteLine("\nEnter the threshold, if any, as a percentage.You can enter any number between 0 and 100.");
+            int thresHold = Convert.ToInt32(Console.ReadLine());
+
+            // User to enter how many seats they want to allocate
+            Console.WriteLine("\nEnter the number of seats to be contested");
+            int seatsCount = Convert.ToInt32(Console.ReadLine());
+
+            // Dhon't method
+            int totalVotes = SumOfVotes(partys);
+            DisplayPercentageVotes(partys, thresHold, totalVotes);
+            CalculateDhondt(partys, seatsCount);
+            DisplayWinningParties(partys);
+
+            Console.ReadKey();
+        }
 
 
 
